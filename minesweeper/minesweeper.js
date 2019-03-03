927f258ffddf3738b84f93954b6824698426928a
William Carlson <wcarlson81@gmail.com>
2019-03-03 17:08:47 -0600

    minecraft mock-up

---
+++ minesweeper/minesweeper.js
@@ -0,0 +1,16 @@
+// sets constants for mocking up the text-based game board
+const blankLine = '  |   |  ';
+const guessLine = '1 |   |  ';
+const bombLine = '  | B |  ';
+
+// builds the blank game board
+console.log('This is what an empty board would look like:');
+console.log(blankLine);
+console.log(blankLine);
+console.log(blankLine);
+
+// builds a game board with simulated user interaction displayed
+console.log('This is what a board with a guess and a bomb on it would look like:');
+console.log(guessLine);
+console.log(bombLine);
+console.log(blankLine);
